eabe1c4bb70a3e92d54775a1e63dc139bf8a4cd1
zylstraj <colbertlove12@gmail.com>
2017-04-07 16:21:17 -0700

    Building routes.
    Diving into Node. Re-learning/playing with server-side routing.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const router = require('./router');
+
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server up on port: ' + PORT));
